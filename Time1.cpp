56fadaab2ee5d5fe7c2cd5c7b17bf2649704000e
Amr Emad <104625612+amremad19801@users.noreply.github.com>
2022-07-27 23:42:33 +0200

    Create Time1.cpp

---
+++ Time1.cpp
@@ -0,0 +1,23 @@
+/*
+    Snake Game implemented using Doubly Linked List in C++.
+*/
+/*
+    We will calculate the execution time of the code before and after changing the parameters, then we will compare the results.
+    We will calculate it using the clock() function.
+    The clock() function in C++ is used to return the approximate processor time that is consumed by the program.
+    In order to compute the processor time, the difference between values returned by two different calls to clock(), one at the start and other at the end of the program is used.
+    To convert the value to seconds, it needs to be divided by a macro CLOCKS_PER_SEC.
+    Each code is written in multi-line comment signs.
+/*
+    Snake Game in C++ Using Doubly Linked List.
+*/
+/*
+    Execution Time:
+    It changes with the width and the hight of the window/box in which the snake moves (The size of the box).
+    It changes with the position of the fruit (fx & fy).
+    It changes with the directions the players (user) take during the game.
+    It changes with the speed of the sanke during the game.
+    It changes with the time the players (user) take during the game.
+*/
+
+//  The original code before Changing he size of the window/box in which the snake moves.
